1b0360a8232b6076e86a42bf348c3a75055ba4ea
Latika Mandal <latikamandal@gmail.com>
2026-07-02 09:37:03 +0530

    Remove Clerk components from Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -2,7 +2,8 @@
 import Link from 'next/link'
 import { useState } from 'react'
 import { Menu, X, MapPin } from 'lucide-react'
-import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { UserButton } from '@clerk/nextjs'
+import { SignedIn, SignedOut } from '@clerk/nextjs/react'
 import { Button } from '@/components/ui/button'
 
 const navLinks = [
